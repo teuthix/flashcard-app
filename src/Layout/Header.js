7567e09463090223b5fdb4f76eadee5bf0a521d6
wey <pukaimakaola@gmail.com>
2024-04-23 22:44:28 -0400

    tweaked header format

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="d-flex bg-dark mb-4 p-4">
+    <header className="d-flex bg-dark mb-4 p-5">
       <div className="container">
         <div className=" text-white">
-          <h1 className="display-4">Flashcard-o-matic</h1>
+          <Link to="/" className="link-light link-underline-opacity-0">
+            <h1 className="display-4">Flashcard-o-matic</h1>
+          </Link>
           <p className="lead">Discover The Flashcard Difference.</p>
         </div>
-        <Link to="/decks/new">
+        <Link to="/decks/new" className="align-self-center">
           <button type="button" className="btn btn-secondary">
             + Create Deck
           </button>
